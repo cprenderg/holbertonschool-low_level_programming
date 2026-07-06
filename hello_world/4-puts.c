bebb3bfceee74e4eddd34e80c7865f8c829115dd
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-06 02:56:36 +0000

    Fixed style hello_world/4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Prints a string
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
