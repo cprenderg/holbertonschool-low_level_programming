ddec0fb5c71f6e35e97ebbad6af4e23ffe12785d
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-06 16:36:11 +1000

    Added variables_if_else_while/4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int i;
+
+	n = 97;
+	for (i = 0; i <= 25; i++)
+	{
+		if (n == 101 || n == 113)
+			n++;
+		else
+		{
+			putchar(n);
+			n++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
