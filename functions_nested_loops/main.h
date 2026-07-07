75ac0425dfef4422b7cd32b19226a5a3864a6f7e
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-07 11:22:38 +1000

    Updated functions_nested_loops/main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,12 +5,19 @@
 #include <stdlib.h>
 
 /**
- * _putchar - writs the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
  * One error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void);
 
 #endif
