af7002a5c448ea9317e36dfbde262d96d365bf43
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-07 10:52:48 +1000

    Added functions_nested_loops/main.h

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _putchar - writs the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * One error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
