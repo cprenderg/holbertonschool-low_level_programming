327447a94025f6349c2ceba9d389209b4a01bdc4
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-09 22:31:45 +1000

    Added functions_nested_loops/10-add.c and updated functions_nested_loops/main.h

---
+++ holbertonschool-low_level_programming/functions_nested_loops/main.h
@@ -0,0 +1,96 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * One error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - prints the alphabet ten times in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void);
+
+/**
+ * _islower - checks if a character is lowercase
+ * @c: character to check
+ *
+ * Return: 1 if lower, 0 if else
+ */
+int _islower(int);
+
+/**
+ * _isalpha - checks if c is a letter
+ * @c: character to check
+ *
+ * Return: 1 if letter, 0 if else
+ */
+int _isalpha(int c);
+
+/**
+ * print_sign - checks if n is positive, negative, or zero
+ * @n: integer to check
+ *
+ * Return: 1 and prints + if n is greater than zero
+ * Return: 0 and prints 0 if n is zero
+ * Return: -1 and prints - if n is less than zero
+ */
+int print_sign(int n);
+
+/**
+ * _abs - computes absolute value of n
+ * @int: integer to check
+ *
+ * Return: The absolute value of n
+ */
+int _abs(int n);
+
+/**
+ * print_last_digit - returns the last digit of n
+ * @n: integer to retrieve the last digit from
+ *
+ * Return: the last digit of n
+ */
+int print_last_digit(int n);
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * @m: for minutes
+ * @h: for hours
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void);
+
+/**
+ * times_table: prints a times table up to 9
+ *
+ * Return: void
+ */
+void times_table(void);
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first integer
+ * @b: second integer
+ * 
+ * Return: sum of a and b
+ */
+int add (int a, int b);
+#endif
