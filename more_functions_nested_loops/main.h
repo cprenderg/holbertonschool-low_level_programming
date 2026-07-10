b94833120a5ced6330cb978da7add8d7e5423a04
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-10 15:55:29 +1000

    Added more_functions_nested_loops/main.h and more_functions_nested_loops/0-isupper.c

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _isupper - checks if c is uppercase
+ * @c: character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c);
+
+#endif
