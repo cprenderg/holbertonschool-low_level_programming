f76a88f167385f9fba4b1daed6ed184c307b747b
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-10 18:23:13 +1000

    Updated more_functions_nested_loops/main.h and Added more_functions_nested_loops/3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0 to 9, followed by a new line
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		_putchar(i + 48);
+		i++;
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -29,4 +29,20 @@ int _isdigit(int c);
  */
 int mul(int a, int b);
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * One error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * print_numbers - prints 0 to 9, followed by a new line
+ *
+ * Return: void
+ */
+void print_numbers(void);
+
 #endif
