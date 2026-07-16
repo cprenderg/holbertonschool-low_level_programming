f64721c0d650e2f2ff0a39adc298c829270d9087
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-16 15:34:28 +1000

    Fixed simple-calculator/calculator.c

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -12,7 +12,6 @@ int main()
 	int answer;
 
 	printf("Simple Calculator\n");
-
 	while (1)
 	{
 		printf("Type one of the following characters to choose an operation:\n");
@@ -22,13 +21,11 @@ int main()
 		printf("'4' to divide\n");
 		printf("'0' to quit\n");
 		scanf("%d", &function);
-		printf("You have selected %d\n", function);
 		if (function >= 0 && function <= 4)
 			break;
 		else
 			printf("Invalid choice\n");
 	}
-
 	if (function == 1)
 	{
 		printf("Please choose your first number:\n");
@@ -45,6 +42,15 @@ int main()
 		printf("Please choose your second number:\n");
 		scanf("%d", &num2);
 		answer = num1 - num2;
+		printf("Result: %d\n", answer);
+	}
+	else if (function == 3)
+	{
+		printf("Please choose your first number:\n");
+		scanf("%d", &num1);
+		printf("Please choose your second number:\n");
+		scanf("%d", &num2);
+		answer = num1 * num2;
 		printf("Result: %d\n", answer);
 	}
 	return (0);
