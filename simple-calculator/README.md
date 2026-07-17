078513d828ec2fcc48ef184af3665b48e1600e2e
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-17 13:02:12 +1000

    Updated simple-calculator/README.md and simple-calculator/calculator.c

--- simple-calculator/README.md
+++ simple-calculator/README.md
@@ -1,2 +1,18 @@
 # C - Simple Calculator
-This directory contains a program that is a simple terminal based calculator. It was completed for the "C - Simple Calculator" task at Holberton.
+## Compile Command
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 calculator.c -o calculator
+
+## Run Instructions
+After compiling with the above flags type ./calculator to run the program
+
+## Supported Operations
+This simple calculator can add, subtract, multiply and divide
+
+## Numeric Behaviour
+No operations accept decimal numbers as input. If the result of division is a decimal it is rounded down to one decimal place.
+
+## Known Limitations
+- Inputting more than 10 characters has undefined behaviour.
+- Inputting a number greater than 2,147,483,647 or less than -2,147,483,648 behaviour is undefined
+- Only runs one calculation per execution
+- If result is greater than 2,147,483,647 or less than -2,147,483,648 behaviour is undefined
--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -6,6 +6,9 @@
  */
 int main()
 {
+	char user_input[11];
+	int user_input_len;
+	int valid;
 	int function;
 	int num1;
 	int num2;
@@ -20,49 +23,250 @@ int main()
 		printf("'3' to multiply\n");
 		printf("'4' to divide\n");
 		printf("'0' to quit\n");
-		scanf("%d", &function);
-		if (function >= 0 && function <= 4)
-			break;
+		scanf("%10s", user_input);
+		
+		user_input_len = 0;
+
+		while (user_input[user_input_len] != '\0')
+			user_input_len++;
+		if (user_input_len == 1)
+		{
+			if (user_input[0] >= '0' && user_input[0] <= '4')
+			{
+				function = user_input[0] - '0';
+				break;
+			}
+			else
+				printf("Invalid choice\n");
+		}
 		else
 			printf("Invalid choice\n");
 	}
+
+	num1 = 0;
+	num2 = 0;
+	valid = 1;
+	if (function == 0)
+		printf("Bye!\n");
 	if (function == 1)
 	{
-		printf("Please choose your first number:\n");
-		scanf("%d", &num1);
-		printf("Please choose your second number:\n");
-		scanf("%d", &num2);
+		while (1)
+		{
+			printf("Please choose your first number:\n");
+			scanf("%10s", user_input);
+			user_input_len = 0;
+			num1 = 0;
+			valid = 1;
+			while (user_input[user_input_len] != '\0')
+			{
+				if (user_input[user_input_len] >= '0' && user_input[user_input_len] <= '9')
+				{
+					num1 = num1 * 10;
+					num1 += user_input[user_input_len] - '0';
+				}
+				else
+				{
+					printf("Invalid choice\n");
+					valid = 0;
+					break;
+				}
+				user_input_len++;
+			}
+			if (valid == 1)
+				break;
+		}
+		while (1)
+		{
+			printf("Please choose your second number:\n");
+			scanf("%10s", user_input);
+			user_input_len = 0;
+			num2 = 0;
+			valid = 1;
+			while (user_input[user_input_len] != '\0')
+			{
+				if (user_input[user_input_len] >= '0' && user_input[user_input_len] <= '9')
+				{
+					num2 = num2 * 10;
+					num2 += user_input[user_input_len] - '0';
+				}
+				else
+				{
+					printf("Invalid choice\n");
+					valid = 0;
+					break;
+				}
+				user_input_len++;
+			}
+			if (valid == 1)
+				break;
+		}
 		answer = num1 + num2;
 		printf("Result: %.0f\n", answer);
 	}
 	else if (function == 2)
 	{
-		printf("Please choose your first number:\n");
-		scanf("%d", &num1);
-		printf("Please choose your second number:\n");
-		scanf("%d", &num2);
+		while (1)
+		{
+			printf("Please choose your first number:\n");
+			scanf("%10s", user_input);
+			user_input_len = 0;
+			num1 = 0;
+			valid = 1;
+			while (user_input[user_input_len] != '\0')
+			{
+				if (user_input[user_input_len] >= '0' && user_input[user_input_len] <= '9')
+				{
+					num1 = num1 * 10;
+					num1 += user_input[user_input_len] - '0';
+				}
+				else
+				{
+					printf("Invalid choice\n");
+					valid = 0;
+					break;
+				}
+				user_input_len++;
+			}
+			if (valid == 1)
+				break;
+		}
+		while (1)
+		{
+			printf("Please choose your second number:\n");
+			scanf("%10s", user_input);
+			user_input_len = 0;
+			num2 = 0;
+			valid = 1;
+			while (user_input[user_input_len] != '\0')
+			{
+				if (user_input[user_input_len] >= '0' && user_input[user_input_len] <= '9')
+				{
+					num2 = num2 * 10;
+					num2 += user_input[user_input_len] - '0';
+				}
+				else
+				{
+					printf("Invalid choice\n");
+					valid = 0;
+					break;
+				}
+				user_input_len++;
+			}
+			if (valid == 1)
+				break;
+		}
 		answer = num1 - num2;
 		printf("Result: %.0f\n", answer);
 	}
 	else if (function == 3)
+	
 	{
-		printf("Please choose your first number:\n");
-		scanf("%d", &num1);
-		printf("Please choose your second number:\n");
-		scanf("%d", &num2);
+		while (1)
+		{
+			printf("Please choose your first number:\n");
+			scanf("%10s", user_input);
+			user_input_len = 0;
+			num1 = 0;
+			valid = 1;
+			while (user_input[user_input_len] != '\0')
+			{
+				if (user_input[user_input_len] >= '0' && user_input[user_input_len] <= '9')
+				{
+					num1 = num1 * 10;
+					num1 += user_input[user_input_len] - '0';
+				}
+				else
+				{
+					printf("Invalid choice\n");
+					valid = 0;
+					break;
+				}
+				user_input_len++;
+			}
+			if (valid == 1)
+				break;
+		}
+		while (1)
+		{
+			printf("Please choose your second number:\n");
+			scanf("%10s", user_input);
+			user_input_len = 0;
+			num2 = 0;
+			valid = 1;
+			while (user_input[user_input_len] != '\0')
+			{
+				if (user_input[user_input_len] >= '0' && user_input[user_input_len] <= '9')
+				{
+					num2 = num2 * 10;
+					num2 += user_input[user_input_len] - '0';
+				}
+				else
+				{
+					printf("Invalid choice\n");
+					valid = 0;
+					break;
+				}
+				user_input_len++;
+			}
+			if (valid == 1)
+				break;
+		}
 		answer = num1 * num2;
 		printf("Result: %.0f\n", answer);
 	}
 	else if (function == 4)
 	{
-		printf("Please choose your first number:\n");
-		scanf("%d", &num1);
-		printf("Please choose your second number:\n");
-		scanf("%d", &num2);
-		if (num2 == 0)
+		while (1)
 		{
-			printf("Error: division by zero\n");
-			return (0);
+			printf("Please choose your first number:\n");
+			scanf("%10s", user_input);
+			user_input_len = 0;
+			num1 = 0;
+			valid = 1;
+			while (user_input[user_input_len] != '\0')
+			{
+				if (user_input[user_input_len] >= '0' && user_input[user_input_len] <= '9')
+				{
+					num1 = num1 * 10;
+					num1 += user_input[user_input_len] - '0';
+				}
+				else
+				{
+					printf("Invalid choice\n");
+					valid = 0;
+					break;
+				}
+				user_input_len++;
+			}
+			if (valid == 1)
+				break;
+		}
+		while (1)
+		{
+			printf("Please choose your second number:\n");
+			scanf("%10s", user_input);
+			user_input_len = 0;
+			num2 = 0;
+			valid = 1;
+			while (user_input[user_input_len] != '\0')
+			{
+				if (user_input[user_input_len] >= '0' && user_input[user_input_len] <= '9')
+				{
+					num2 = num2 * 10;
+					num2 += user_input[user_input_len] - '0';
+				}
+				else
+				{
+					printf("Invalid choice\n");
+					valid = 0;
+					break;
+				}
+				user_input_len++;
+			}
+			if (num2 == 0)
+				valid = 0;
+			if (valid == 1)
+				break;
 		}
 		answer = (float)num1 / num2;
 		printf("Result: %.1f\n", answer);
