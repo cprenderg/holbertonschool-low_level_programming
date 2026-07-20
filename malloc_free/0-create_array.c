93b4932ef384041293a3926211d3461b11aa59b2
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-20 11:15:45 +1000

    Added malloc_free/main.h and malloc_free/0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_array - creates a array of chars, initialised with c
+ * @size: size of the array
+ * @c: char to fill array with
+ *
+ * Return: pointer to created array, or NULL if size = 0 or if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(size * sizeof(char));
+	while (i < size)
+	{
+		array[i] = c;
+		i++;
+	}
+	return (array);
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,2 @@
+# C - malloc, free
+This directory contains the programs used for the "C - malloc, free" module at Holberton.
