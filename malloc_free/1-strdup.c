1ab68d44a31fa949d7bb4d019df3ce49e2a63506
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-20 13:03:17 +1000

    Fixed malloc_free/1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,16 +11,18 @@ char *_strdup(char *str)
 	int i;
 	char *copied_str;
 
+	if (str == NULL)
+		return (NULL);
 	len = 0;
 	while (str[len] != '\0')
 		len++;
 	if (len == 0)
 		return (NULL);
-	copied_str = malloc(len * sizeof(char));
+	copied_str = malloc((len + 1) * sizeof(char));
 	if (copied_str == NULL)
 		return (NULL);
 	i = 0;
-	while (i < len || str[i] == '\0')
+	while (i <= len)
 	{
 		copied_str[i] = str[i];
 		i++;
