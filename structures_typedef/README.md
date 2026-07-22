ca555c340d1279b9e592c74edda6aa8980635781
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-22 14:45:27 +1000

    Added structures_typedef/README.md and structures_typedef/dog.h

---
+++ structures_typedef/README.md
@@ -0,0 +1,2 @@
+# C - Structures, typedef
+This directory contains the programs used for the "C - Structures, typedef" module at Holberton.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#define DOG_H
+
+/**
+ * struc dog - information about a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
