0b34952911d62e86cd6c7ce443cdeead89bae56d
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-27 16:07:22 +1000

    Updated variadic_functions/variadic_functions.h and added variadic_functions/2-print_strings.c

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints string followed by a new line
+ * @separator: what should separate each string
+ * @n: amount of strings
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	i = 0;
+	va_start(ap, n);
+	while (i < n)
+	{
+		i++;
+		str = va_arg(ap, char *);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+
+		}
+		if (i == n)
+		{
+			break;
+		}
+		else
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -19,4 +19,13 @@ int sum_them_all(const unsigned int n, ...);
  * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...);
+
+/**
+ * print_strings - prints string followed by a new line
+ * @separator: what should separate each string
+ * @n: amount of strings
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...);
 #endif
