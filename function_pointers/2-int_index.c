6a9c85d6c43d5576336ba5a01bc2089207cb7868
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-27 12:27:27 +1000

    Fixed function_pointers/2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,7 +14,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	i = 0;
 	if (array == NULL || cmp == NULL)
 	{
-		return (NULL);
+		return (-1);
+	}
 	while (i < size)
 	{
 		if (cmp(array[i]) != 0)
