59b04672468bdc78c7688f22cff9f4e2b82ca457
Christopher Prendergast <14800@holbertonstudents.com>
2026-07-27 12:07:57 +1000

    Updated function_pointers/function_pointers.h and added function_pointers/1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: array to iterate over
+ * @size: size of array
+ * @action: function to execute on each element
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,4 +11,13 @@
  */
 void print_name(char *name, void (*f)(char *));
 
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: array to iterate over
+ * @size: size of array
+ * @action: function to execute on each element
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
