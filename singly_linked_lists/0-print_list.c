b592a3b3b3c25fabb7b38c13276e39fcdf699473
Christopher Prendergast <14800@holbertonstudents.com>
2026-08-04 13:06:30 +1000

    Added 0-print_list.c, README.md and lists.h

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: list_t list to print
+ *
+ * Return: amount of linked lists printed
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *current_list;
+	long unsigned int elements;
+
+	current_list = h;
+	elements = 0;
+	while (current_list != NULL)
+	{
+		if (current_list->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] [%s]\n", current_list->len, current_list->str);
+		}
+		current_list = current_list->next;
+		elements++;
+	}
+	return (elements);
+}
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# C - Singly linked lists
+This directory contains the programs used for the "C - Singly linked lists" module at Holberton.
+
