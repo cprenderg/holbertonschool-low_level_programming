d8bc1dcae93d9879c59769bdd03728f63a6d45e0
Christopher Prendergast <14800@holbertonstudents.com>
2026-08-04 14:49:19 +1000

    Updated lists.h and added 4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: head of list_t list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
