4efd12b0236737c851302148784474623372a7c9
Christopher Prendergast <14800@holbertonstudents.com>
2026-08-10 13:47:09 +1000

    Fixed inserting new_node at head

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -42,6 +42,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->prev = current_node->prev;
 		current_node->prev->next = new_node;
 	}
+	else
+	{
+		*h = new_node;
+	}
 	new_node->next = current_node;
 	current_node->prev = new_node;
 	return (new_node);
