d695004e28bb1f4b1b8064df1c0535848f916576
Christopher Prendergast <14800@holbertonstudents.com>
2026-08-10 16:45:37 +1000

    Fixed style

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,12 +10,14 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	char *str;
+	char *key1;
 	hash_node_t *new_node;
 	int index;
 
 	if (ht == NULL)
 		return (0);
 	str = strdup(value);
+	key1 = strdup(key);
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
 	{
@@ -23,19 +25,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	index = (hash_djb2((const unsigned char *)key) % ht->size);
-	if (new_node->key != strdup(key))
-	{
-		new_node->key = strdup(key);
-		if (ht->array[index] != NULL)
-		{
-			new_node->next = ht->array[index];
-		}
-		else
-		{
-			new_node->next = NULL;
-		}
-	}	
-	ht->array[index] = new_node;
+	new_node->key = key1;
 	new_node->value = str;
+	if (ht->array[index] == NULL)
+	{
+		new_node->next = NULL;
+		ht->array[index] = new_node;
+	}
+	else if (strcmp(key1, ht->array[index]->key) == 0)
+	{
+		ht->array[index]->value = str;
+		return (1);
+	}
+	else if (strcmp(key1, ht->array[index]->key) != 0)
+	{
+		if (ht->array[index] != NULL)
+			new_node->next = ht->array[index];
+		else
+			new_node->next = NULL;
+		ht->array[index] = new_node;
+	}
 	return (1);
 }
