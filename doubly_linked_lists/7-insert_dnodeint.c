49a52ce941e1dcf231eba54d18b4391a077ddc16
Christopher Prendergast <14800@holbertonstudents.com>
2026-08-10 14:25:24 +1000

    Fixed adding element at end

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -38,7 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (i != idx)
 		return (NULL);
-	if (current_node->next == NULL)
+	if (current_node == NULL)
 	{
 		new_node = add_dnodeint_end(h, n);
 		return (new_node);
