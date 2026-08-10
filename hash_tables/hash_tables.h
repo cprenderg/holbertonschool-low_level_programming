6b12527cc0becdc85fb9ccd759683c76976bf71d
Christopher Prendergast <14800@holbertonstudents.com>
2026-08-10 15:34:30 +1000

    Fixed style

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -15,11 +14,10 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
-
 /**
  * struct hash_table_s - Hash table data structure
  *
@@ -30,8 +28,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
