d3a224f23fc25a7fb58b13062f06a65ba0670b3f
Christopher Prendergast <14800@holbertonstudents.com>
2026-08-12 12:06:30 +1000

    Updated main.h and added 3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - returns factorial of a given number
+ * @n: integer to factorial
+ *
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n <= 0)
+		return (-1);
+	else if (n == 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
