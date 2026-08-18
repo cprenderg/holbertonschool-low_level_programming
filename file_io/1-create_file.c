0ef6c3e218311c71a5c6be5eb090cdd76f924115
Christopher Prendergast <14800@holbertonstudents.com>
2026-08-18 14:21:12 +1000

    Updated main.h and added 1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - creates a file and writes text to it
+ * @filename: name of the file to create
+ * @text_content: text to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+	size_t length;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		length = 0;
+		while (text_content[length] != '\0')
+			length++;
+
+		bytes_written = write(fd, text_content, length);
+		if (bytes_written != (ssize_t)length)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
